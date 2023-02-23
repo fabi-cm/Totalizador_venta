b8a1c776a5eda54667b3e254d4f742d08343ae18
Fabio Copa Mejia <dahofcmmejia@gmail.com>
2023-02-23 04:48:50 -0400

    mostrar estado

--- src/presenter.js
+++ src/presenter.js
@@ -8,9 +8,14 @@ const div_cantidad = document.querySelector("#cantidad-item-div");
 const precio = document.querySelector("#precio-item");
 const div_precio = document.querySelector("#precio-item-div");
 
+const codigo_estado = document.querySelector("#codigo-estado");
+const div_codigo_estado = document.querySelector("#codigo-estado-div"); 
+
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   div_cantidad.innerHTML = "<p> Cantidad de item: " + cantidad.value + "</p>";
   div_precio.innerHTML = "<p> Precio de item: " + precio.value + "</p>";
+  div_codigo_estado.innerHTML = "<p> Codigo del Estado de "+ codigo_estado.value + "</p>";
 });
