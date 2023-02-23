a49acc51b89770d264af924fda3345ce6a7e12e1
Fabio Copa Mejia <dahofcmmejia@gmail.com>
2023-02-23 05:26:58 -0400

    mostrar el valor del impuesto segun precio neto y porcentaje

--- src/impuesto.js
+++ src/impuesto.js
@@ -1,11 +1,4 @@
-function porcentaje_estado(estado) {
-    switch(estado){
-      case 'UT': return 6.65;break;
-      case 'NV': return 8;break;
-      case 'TX': return 6.25;break;
-      case 'AL': return 4; break;
-      case 'CA': return 8.25;break;
-    }
+function impuesto(precio_neto, codigo_estado) {
+    return (precio_neto*codigo_estado)/100;
 }
-export default porcentaje_estado;
-  
+export default impuesto;
--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,6 @@
-import impuesto from "./impuesto";
+import porcentaje_estado from "./porcentaje_estado";
 import descuento from "./descuento";
+import impuesto from "./impuesto";
 
 const form = document.querySelector("#totalizador-form");
 const cantidad = document.querySelector("#cantidad-item");
@@ -8,21 +9,23 @@ const codigo_estado = document.querySelector("#codigo-estado");
 
 const div_cantidad = document.querySelector("#cantidad-item-div");
 const div_precio = document.querySelector("#precio-item-div");
-const div_codigo_estado = document.querySelector("#codigo-estado-div"); 
+const div_codigo_estado = document.querySelector("#codigo-estado-div");
 const div_precio_neto = document.querySelector("#precio-neto-div");
 const div_descuento = document.querySelector("#precio-descuento-div");
+const div_impuesto = document.querySelector("#impuesto-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  var cantidad_impuesto = impuesto(codigo_estado.value)
+  var porcentaje_impuesto = porcentaje_estado(codigo_estado.value);
   var precio_neto = precio.value * cantidad.value;
   var cantidad_descuento = descuento(cantidad.value);
-  
+  var cantidad_impuesto = impuesto(precio_neto, porcentaje_impuesto);
+
   div_cantidad.innerHTML = "<p> Cantidad de item: " + cantidad.value + "</p>";
   div_precio.innerHTML = "<p> Precio de item: " + precio.value + "</p>";
-  div_codigo_estado.innerHTML = "<p> Codigo del Estado de "+ codigo_estado.value +": " + cantidad_impuesto + "%</p>";
-  div_precio_neto.innerHTML = "<p> Precio Neto:("+cantidad.value+"*$"+precio.value+"):$"+ precio_neto + "</p>";
-  div_descuento.innerHTML = "<p> Porcentaje de Descuento: "+ cantidad_descuento + "%</p>";
+  div_codigo_estado.innerHTML ="<p> Codigo del Estado de " +codigo_estado.value +": " +porcentaje_impuesto +"%</p>";
+  div_precio_neto.innerHTML ="<p> Precio Neto:(" +cantidad.value +"*$" +precio.value +"):$" +precio_neto +"</p>";
+  div_descuento.innerHTML ="<p> Porcentaje de Descuento: " + cantidad_descuento + "%</p>";
+  div_impuesto.innerHTML ="<p> Impuesto para " +codigo_estado.value +"(" +porcentaje_impuesto +"): $" +cantidad_impuesto +"</p>";
 });
-
